393b9e6abce0772fd3808aae02705fdf2ada6031
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-03-14 13:57:16 -0300

    Have rails resolver handle lib files

--- spec/mj/alternative_file/resolvers/ruby/rails_resolver_spec.rb
+++ spec/mj/alternative_file/resolvers/ruby/rails_resolver_spec.rb
@@ -11,21 +11,63 @@ RSpec.describe Mj::AlternativeFile::Resolvers::Ruby::RailsResolver do
     expect(resolver).to be_a(Mj::AlternativeFile::Resolvers::Base)
   end
 
-  it "resolves from spec file" do
-    result = resolve("spec/models/users/profile_spec.rb")
+  context "with rails models" do
+    it "resolves from spec file" do
+      result = resolve("spec/models/users/profile_spec.rb")
 
-    expect(result).to include(create_candidate("app/models/users/profile.rb", "model"))
+      expect(result).to include(create_candidate("app/models/users/profile.rb", "model"))
+    end
+
+    it "resolves rspec file" do
+      result = resolve("app/models/users/profile.rb")
+
+      expect(result).to include(create_candidate("spec/models/users/profile_spec.rb", "spec"))
+    end
+
+    it "resolves minitest file" do
+      result = resolve("app/models/users/profile.rb")
+
+      expect(result).to include(create_candidate("test/models/users/profile_test.rb", "minitest"))
+    end
   end
 
-  it "resolves rspec file" do
-    result = resolve("app/models/users/profile.rb")
+  context "with rails other app/folders" do
+    it "resolves from spec file" do
+      result = resolve("spec/services/users/profile_spec.rb")
 
-    expect(result).to include(create_candidate("spec/models/users/profile_spec.rb", "spec"))
+      expect(result).to include(create_candidate("app/services/users/profile.rb", "model"))
+    end
+
+    it "resolves rspec file" do
+      result = resolve("app/services/users/profile.rb")
+
+      expect(result).to include(create_candidate("spec/services/users/profile_spec.rb", "spec"))
+    end
+
+    it "resolves minitest file" do
+      result = resolve("app/services/users/profile.rb")
+
+      expect(result).to include(create_candidate("test/services/users/profile_test.rb", "minitest"))
+    end
   end
 
-  it "resolves minitest file" do
-    result = resolve("app/models/users/profile.rb")
+  context "with files that belong in lib" do
+    it "resolves from spec file" do
+      result = resolve("spec/services/users/profile_spec.rb")
 
-    expect(result).to include(create_candidate("test/models/users/profile_test.rb", "minitest"))
+      expect(result).to include(create_candidate("lib/services/users/profile.rb", "lib"))
+    end
+
+    it "resolves rspec file" do
+      result = resolve("lib/services/users/profile.rb")
+
+      expect(result).to include(create_candidate("spec/services/users/profile_spec.rb", "spec"))
+    end
+
+    it "resolves minitest file" do
+      result = resolve("lib/services/users/profile.rb")
+
+      expect(result).to include(create_candidate("test/services/users/profile_test.rb", "minitest"))
+    end
   end
 end

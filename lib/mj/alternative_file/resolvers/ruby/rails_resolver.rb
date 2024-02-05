3ce9918c3efbc13be2c088d74c1dec968e6decd1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-02-05 13:14:38 -0300

    Refactor and likely fix vendored gems resolver

--- lib/mj/alternative_file/resolvers/ruby/rails_resolver.rb
+++ lib/mj/alternative_file/resolvers/ruby/rails_resolver.rb
@@ -5,23 +5,34 @@ module Mj
     module Resolvers
       module Ruby
         class RailsResolver < Resolvers::Base
+          attr_reader :root
+
+          def initialize(root: nil)
+            if root
+              root = "#{root.chomp("/")}/"
+            end
+
+            @root = root
+          end
+
           private
 
           def apply_to?(file)
             file.extension == "rb"
           end
 
-          def add_candidates(file, candidates)
-            ruby_file = Ruby::RubyFile.new(file)
-            add_candidate("app/#{ruby_file.class_path}.rb", :model, to: candidates)
-            add_candidate("spec/#{ruby_file.class_path}_spec.rb", :spec, to: candidates)
-            add_candidate("test/#{ruby_file.class_path}_test.rb", :minitest, to: candidates)
+          def add_candidates(file, candidates) # rubocop:disable Metrics/AbcSize
+            ruby_file = Ruby::RubyFile.new(file).without_prefix(root)
+
+            add_candidate("#{root}app/#{ruby_file.class_path}.rb", :model, to: candidates)
+            add_candidate("#{root}spec/#{ruby_file.class_path}_spec.rb", :spec, to: candidates)
+            add_candidate("#{root}test/#{ruby_file.class_path}_test.rb", :minitest, to: candidates)
 
             # lib files
-            add_candidate("lib/#{ruby_file.class_path}.rb", "lib", to: candidates)
-            add_candidate("#{ruby_file.class_path}.rb", "lib", to: candidates)
-            add_candidate("spec/lib/#{ruby_file.class_path}_spec.rb", :spec, to: candidates)
-            add_candidate("test/lib/#{ruby_file.class_path}_test.rb", :minitest, to: candidates)
+            add_candidate("#{root}lib/#{ruby_file.class_path}.rb", "lib", to: candidates)
+            add_candidate("#{root}#{ruby_file.class_path}.rb", "lib", to: candidates)
+            add_candidate("#{root}spec/lib/#{ruby_file.class_path}_spec.rb", :spec, to: candidates)
+            add_candidate("#{root}test/lib/#{ruby_file.class_path}_test.rb", :minitest, to: candidates)
           end
         end
       end
--- lib/mj/alternative_file/resolvers/ruby/vendored_gems_resolver.rb
+++ lib/mj/alternative_file/resolvers/ruby/vendored_gems_resolver.rb
@@ -5,33 +5,17 @@ module Mj
     module Resolvers
       module Ruby
         class VendoredGemsResolver < Resolvers::Base
-          def initialize
-            @rails = RailsResolver.new
-          end
-
           private
 
           def apply_to?(file)
             file.extension == "rb" && find_root(file)
           end
 
-          def add_candidates(file, candidates) # rubocop:disable Metrics/MethodLength
-            root = find_root(file)
+          def add_candidates(file, candidates)
+            @rails = RailsResolver.new(root: find_root(file))
 
             @rails.resolve(file).each do |resolved|
-              found = resolved.to_s.sub("#{root}/app/", "")
-              found = found.sub("#{root}/", "")
-
-              unless found.match?("_spec.rb")
-                found = found.sub("spec/", "")
-              end
-
-              unless found.match?("_test.rb")
-                found = found.sub("test/", "")
-              end
-
-              file = [root, found].flatten.join("/")
-              add_candidate(file, resolved.type, to: candidates)
+              add_candidate(resolved, resolved.type, to: candidates)
             end
           end
 

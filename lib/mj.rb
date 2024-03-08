62bd98532c8e2a74a4a47ec2a61ab2e28a11138f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-03-08 16:03:11 -0300

    Implement ChatGPT command

--- lib/mj.rb
+++ lib/mj.rb
@@ -2,6 +2,7 @@
 
 require_relative "mj/version"
 require_relative "mj/cli"
+require "mj/hash_utils"
 
 module Mj
   class Error < StandardError; end
--- lib/mj/chatgpt/commands/ask_command_handler.rb
+++ lib/mj/chatgpt/commands/ask_command_handler.rb
@@ -1,11 +1,23 @@
+# frozen_string_literal: true
+
+require "openai"
+require_relative "../request"
+
 module Mj
   module ChatGpt
     module Commands
       class AskCommandHandler
+        def initialize(client:)
+          @client = client
+        end
+
         def handle(command)
-          return {
-            question: command.question,
-            options: command.options
+          request = Request.from_config_file(command.options.fetch("config_file"))
+          @client.chat(**request.asking(command.question).to_h)
+        rescue Faraday::Error => exception
+          {
+            error: exception.message,
+            body: exception.response&.dig(:body, "error") || exception.response
           }
         end
       end

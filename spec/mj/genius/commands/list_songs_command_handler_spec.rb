18d7bcedcdfd6da498db38cf6d0af5daf9d13059
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-07-25 09:29:32 -0300

    Add integration test
    Covers happy path.

--- spec/mj/genius/commands/list_songs_command_handler_spec.rb
+++ spec/mj/genius/commands/list_songs_command_handler_spec.rb
@@ -1,11 +1,19 @@
-RSpec.describe Mj::Genius::Commands::ListSongsCommandHandlerSpec do
-  let(:command) { Commands::ListSongs.new(artist: "Mallu Magalhães", options: {}) }
+# frozen_string_literal: true
+
+RSpec.describe Mj::Genius::Commands::ListSongsCommandHandler do
+  subject(:handler) { described_class.new(stdout: $stdout, api_client: api_client) }
+
+  let(:command) { Mj::Genius::Commands::ListSongs.new(artist: "Mallu Magalhães", options: {}) }
   let(:songs) { handler.handle(command).sort_by(&:title) }
-  let(:handler)  { Commands::ListSongsCommandHandler.new(stdout: $stdout, api_client: api_client) }
+  let(:api_client) { Mj::Genius::ApiClient.new(access_token: ENV.fetch("GENIUS_ACCESS_TOKEN")) }
 
   describe "integration test" do
-    expect(songs.length).to eq(92)
-    expect(songs.first.title).to eq("America Latina")
-    expect(songs.last.title).to eq("Ô, Ana")
+    it "fetches songs" do
+      VCR.use_cassette("list_songs_command_handler_spec") do
+        expect(songs.length).to eq(92)
+        expect(songs.first.title).to eq("America Latina")
+        expect(songs.last.title).to eq("Ô, Ana")
+      end
+    end
   end
 end
